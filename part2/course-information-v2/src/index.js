c65c0f1f2ab0ab32ff53624e4d12b8c2b70026fe
Ahmed Ezzat <moreone25@yahoo.com>
2020-07-23 14:13:16 +0200

    2.4: Course information step9

--- part2/course-information-v2/src/index.js
+++ part2/course-information-v2/src/index.js
@@ -32,7 +32,7 @@ const Total = ({ parts }) => {
   return (
     <div>
       <p>
-        Total:
+        Total Exercises:
         {parts.reduce((s, p) => {
           return s + p.exercises;
         }, 0)}
@@ -52,39 +52,63 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3,
-      },
-      {
-        name: 'Another part',
-        exercises: 15,
-        id: 4,
-      },
-      {
-        name: 'Yet another part',
-        exercises: 10,
-        id: 5,
-      },
-    ],
-  };
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Another part',
+          exercises: 15,
+          id: 4,
+        },
+        {
+          name: 'Yet another part',
+          exercises: 10,
+          id: 5,
+        },
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
-  return <Course course={course} />;
+  return (
+    <div>
+      {courses.map((course) => (
+        <Course course={course} />
+      ))}
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
